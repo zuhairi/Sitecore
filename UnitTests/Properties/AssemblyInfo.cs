e272e7a64d96889bb57d4f88697c7ec64fd2261c
zuhairi <zuhairi86@gmail.com>
2019-07-07 22:06:59 +0800

    Add files via upload

---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0e96a66f-e0bb-460a-8cc0-37ccdb373a6c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/Server_UnitTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Server_UnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = @"C:\TFS\Sitecore\Sitecore\WindowsPublishSubscribe\bin\Debug\WindowsPublishSubscribe.exe";
+            info.Arguments = @"/server SitecorePipe";
+
+            //Process server = ;
+            Assert.IsTrue(!Process.Start(info).HasExited);
+        }
+    }
+}
